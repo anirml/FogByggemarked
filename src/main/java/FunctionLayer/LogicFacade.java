a5b0ed42f952497917d2f7545da02a84a77a2bc2
slamrebjerg <cph-oh50@cphbusiness.dk>
2019-05-09 11:38:51 +0200

    Rettet i tegning og tilføjet forslag til skur - oh

--- src/main/java/FunctionLayer/LogicFacade.java
+++ src/main/java/FunctionLayer/LogicFacade.java
@@ -40,7 +40,7 @@ public class LogicFacade {
         //arrayPost[2] = remlænge på 1. rem
         //arrayPost[3] = remlænge på del 2
         int [] arrayPost = new int [4];
-        final int POSTDIST_START = 900;
+        final int POSTDIST_START = 600;
         final int POSTDIST_END = 300;
         if (len>=2400&&len<=3900) {
             arrayPost[0]=2;
@@ -81,35 +81,55 @@ public class LogicFacade {
         }
         arrayStern[2] = 2;
         arrayStern[6] = len+300;
-        if (len>5700){
+        if (len>5700) {
             arrayStern[3] = 2;
-            arrayStern[6] = (((len-300)/2)/300+1)*300;
-            arrayStern[7] = arrayStern[6]+300;
+            arrayStern[6] = (((len - 300) / 2) / 300 + 1) * 300;
+            arrayStern[7] = arrayStern[6] + 300;
         }
         return arrayStern;
     }
 
-/*
+    public static int[] roofFlat(int len, int wid) {
+        //Plastmo Ecolite tag
+        int plastmoWid = 1090;
+        int[] plastmoLen = {2400, 3000, 3600, 4200, 4800, 6000};
+        // arrayRoofFlat[0] = pladelængden
+        // arrayRoofFlat[1] = antal plader i længderetning
+        // arrayRoofFlat[2] = antal plader i bredderetning
+        int[] arrayRoofFlat = new int[3];
+        int i = 5;
+        while (plastmoLen[i] > len + 200) {
+            arrayRoofFlat[0] = plastmoLen[i];
+            i = i - 1;
+        }
+        arrayRoofFlat[1] = 1;
+        arrayRoofFlat[2] = (wid ) / 890 + 1;
 
-    public static HashMap<Integer, ItemForList> xMap (List tempWoodList, int dim1, int dim2, int i){
+        if (len>5700){
+            i = 5;
+            while (plastmoLen[i] > len/2 + 200) {
+                arrayRoofFlat[0] = plastmoLen[i];
+                i = i - 1;
+            }
+            arrayRoofFlat[1] = 2;
 
-        List<Wood> woodList = DBAccess.ItemMapper.readWoodList();
-        HashMap<Integer, ItemForList> xMap = new HashMap<>();
-        ItemForList tempWood = new ItemForList();
-        String tempDesc = "";
-
-        if ((woodList.get(i).getWoodDim1() == dim1) && (woodList.get(i).getWoodDim2() == dim2)) {
-            tempDesc = Integer.toString(woodList.get(i).getWoodDim1()) + "x" +
-                    Integer.toString(woodList.get(i).getWoodDim2()) + " mm " +
-                    woodList.get(i).getWoodDesc();
-
-            tempWood = new ItemForList(tempDesc,
-                    Integer.toString(woodList.get(i).getWoodLength()),
-                    woodList.get(i).getWoodUnit());
-            xMap.put(woodList.get(i).getWoodLength(), tempWood);
         }
 
-        return xMap;
+        return arrayRoofFlat;
+
     }
-*/
+
+    public static double[] shedWall (int dim) {
+        //arrayShed[0] = n antal gange 2 brædder
+        //arrayShed[1] = x afstand mellemrum brædder
+        //arrayShed[2] = start afstand
+        double[] arrayShed = new double[3];
+        arrayShed[0] = ((dim-300)-(dim-300)%150)/150+1;
+        arrayShed[1] = (dim-100)/arrayShed[0]-100;
+        arrayShed[2] = 50 + arrayShed[1]/2;
+
+        return arrayShed;
+    }
+
+
 }
